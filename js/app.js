32a5084f8240db065430fb565641cb13bb1af1de
iye87 <iye.martinez@aol.com>
2017-12-01 00:46:52 -0400

    Signed-off-by: iye87 <iye.martinez@aol.com>

--- js/app.js
+++ js/app.js
@@ -205,3 +205,24 @@ function calcularTiempoDosFechas(date1, date2){
   var formatted = ((HH < 10)?("0" + HH):HH) + ":" + ((MM < 10)?("0" + MM):MM)
   return formatted;
 }
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("386ee07f-b554-45c8-a24b-97dc580555aa")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
